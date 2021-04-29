249badb1a0975d67a8f03eec312b2c802c609196
Juergen Hoetzel <juergen@hoetzel.info>
2021-04-29 20:28:04 +0200

    Use tools-cache

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
 const exec = require('@actions/exec');
-const httpm = require('@actions/http-client');
 const os = require('os');
 const fs = require('fs');
 const {join} = require('path');
 
 async function run() {
-    const http = new httpm.HttpClient('setup-eldev-emacs');
     const isWindows = os.type() === 'Windows_NT';
     const webinstall_script = 'eldev' + (isWindows? ".bat" :"");
     const url = `https://raw.github.com/doublep/eldev/master/webinstall/${webinstall_script}`;
     try {
 	core.startGroup('Fetch Eldev');
-	const tmpdir = await fs.promises.mkdtemp(join(os.tmpdir(), 'eldev-'));
-	const localScript = join(tmpdir, webinstall_script);
-	const response = await http.get(url);
-	if (response.message.statusCode != 200)  {
-	    core.setFailed(`error occured bootstraping from ${url}: ${response.message.statusCode}`);
-	    return;
-	}
-	await fs.promises.writeFile(localScript, await response.readBody());
+	const localScript = await tc.downloadTool(url, join(os.tmpdir(), webinstall_script));
 	if (!isWindows) {
 	    await fs.promises.chmod(localScript, fs.constants.S_IXUSR | fs.constants.S_IWUSR | fs.constants.S_IRUSR);
 	}
